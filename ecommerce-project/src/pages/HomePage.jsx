7907fc6d09267bb91cda36805708b8fbc7925a1d
David Werbin <116547196+davidrwerbin@users.noreply.github.com>
2025-11-24 17:22:11 +0200

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,8 +3,17 @@ import { Header } from '../components/Header'
 import { products } from '../../starting-code/data/products'
 
 export function HomePage() {
+
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return(response.json());
+        }).then((data) => {
+            console.log(data);
+        })
+        
   return (
     <>
+
       <title>Ecommerce Project</title>
 
       <Header/>
