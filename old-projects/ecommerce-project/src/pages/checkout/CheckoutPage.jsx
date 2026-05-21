4ee6fb8a930d803d969293848232bad9dcb6c1ae
David Werbin <116547196+davidrwerbin@users.noreply.github.com>
2026-05-21 18:03:48 +0300

    Convert ecommerce project to TS

---
+++ old-projects/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./CheckoutPage.css";
+import { CheckoutHeader } from "./CheckoutHeader";
+import { OrderSummary } from "./OrderSummary";
+import { PaymentSummary } from "./PaymentSummary";
+
+export function CheckoutPage({ cart, loadCart }) {
+  const [deliveryOptions, setDeliveryOptions] = useState([]);
+  const [paymentSummary, setPaymentSummary] = useState(null);
+
+  useEffect(() => {
+    const fetchCheckoutData = async () => {
+      let response = await axios.get(
+        "/api/delivery-options?expand=estimatedDeliveryTime",
+      );
+      setDeliveryOptions(response.data);
+
+      response = await axios.get("/api/payment-summary");
+      setPaymentSummary(response.data);
+    };
+
+    fetchCheckoutData();
+  }, [cart]);
+
+  return (
+    <>
+      <title>Checkout</title>
+
+      <CheckoutHeader />
+
+      <div className="checkout-page">
+        <div className="page-title">Review your order</div>
+
+        <div className="checkout-grid">
+          <OrderSummary
+            cart={cart}
+            deliveryOptions={deliveryOptions}
+            loadCart={loadCart}
+          ></OrderSummary>
+
+          <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart}></PaymentSummary>
+        </div>
+      </div>
+    </>
+  );
+}
